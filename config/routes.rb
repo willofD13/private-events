46d11f687be9736c16a27fafd7349f79e5e53448
willofD13 <leonidastsiakas13@gmail.com>
2023-11-22 20:27:59 +0200

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "events/#index"
+  root "events#index"
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
