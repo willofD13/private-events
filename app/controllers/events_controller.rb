5a3b48582ceb6f37ee484f0c878cec899b77f678
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 17:02:39 +0200

    Create an event linked to a user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,25 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @user = User.find(current_user_id)
+
+    @event = @user.created_events.build(event_params)
+
+    if @event.create
+      redirect_to event_path(@event)
+    else
+      redirect_to :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:date,:location,:creator_id)
+  end
+
 end
