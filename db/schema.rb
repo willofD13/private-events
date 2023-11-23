cc4f021b014e5842e9d581be7b2630193b6b2d1d
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 15:59:41 +0200

    Add creator reference to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_181935) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_134153) do
   create_table "events", force: :cascade do |t|
     t.string "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_181935) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
