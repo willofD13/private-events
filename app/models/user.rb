0c1866cb94f260f4cb9d854ae416186004502ae3
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 17:53:04 +0200

    Delete mail and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,4 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
 
   validates :username, presence: true,uniqueness: true, length: { minimum: 5}
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 5}
 end
