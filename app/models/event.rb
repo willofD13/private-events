9570d134567ca3e9aa4749c8471d528089d13761
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 20:18:10 +0200

    Define the through assocciation first

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
-    has_many :attendees, through: :event_attendings
     has_many :event_attendings, foreign_key: :attended_event_id
+    has_many :attendees, through: :event_attendings
+    
 
 
     validates :title, presence: true
