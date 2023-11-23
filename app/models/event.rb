9b8e7290756ff602c0b719010b7c02223e18fa6c
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 17:37:13 +0200

    Validate event data

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
+
+    validates :title, presence: true
+    validates :body, presence: true
+    validates :date, presence: true
+    validates :location, presence: true
 end
