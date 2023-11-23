31907628958689b97733b193bc44112155db0f91
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 20:17:38 +0200

    Change assocciations order

--- app/models/event_attending.rb
+++ app/models/event_attending.rb
@@ -1,4 +1,4 @@
 class EventAttending < ApplicationRecord
-    belongs_to :attendees, class_name: "User"
     belongs_to :attended_events, class_name: "Event"
+    belongs_to :attendees, class_name: "User"
 end
