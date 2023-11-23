40c00df734083b70708f2c6a4ba169926078496f
willofD13 <leonidastsiakas13@gmail.com>
2023-11-23 17:35:23 +0200

    Validate user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+
+  validates :username, presence: true,uniqueness: true, length: { minimum: 5}
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5}
 end
