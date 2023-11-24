6e1b1ae1dad347cd96515f7a05fc7d6722acb3a4
willofD13 <leonidastsiakas13@gmail.com>
2023-11-24 19:35:48 +0200

    Map an event with its attendee

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -6,4 +6,19 @@ class EventAttendingsController < ApplicationController
   def new
     @event_attending = EventAttending.new
   end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @event_attending = @event.event_attendings.create(event_attending_params)
+
+    if @event_attending.save
+      redirect_to root_path
+    else
+      redirect_to events_path, status: :unprocessable_entity
+    end
+  end
+
+  def event_attending_params
+    params.require(:event_attending).permit(:attended_event_id,:event_attendee_id)
+  end
 end
