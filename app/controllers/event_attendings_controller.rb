213597f3e04fce09b958134608641398275934b5
willofD13 <leonidastsiakas13@gmail.com>
2023-11-24 16:27:04 +0200

    Create index and new actions

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,4 +1,9 @@
 class EventAttendingsController < ApplicationController
   def index
+    @event_attendings = EventAttending.all
+  end
+
+  def new
+    @event_attending = EventAttending.new
   end
 end
