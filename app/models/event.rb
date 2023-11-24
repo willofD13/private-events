c07ea22e5c243f7f94b4338ad0c82f47de6d0fa4
willofD13 <leonidastsiakas13@gmail.com>
2023-11-24 14:03:01 +0200

    Specify where to look for attendees through source

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :event_attendings, foreign_key: :attended_event_id
-    has_many :attendees, through: :event_attendings
+    has_many :attendees, through: :event_attendings, source: :event_attendee
     
-
 
     validates :title, presence: true
     validates :body, presence: true
