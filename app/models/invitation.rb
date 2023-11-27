8648046c86346121e66b0b09e12a98f5f45a6133
willofD13 <leonidastsiakas13@gmail.com>
2023-11-27 20:40:31 +0200

    Create association between user and invitation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,6 @@
 class Invitation < ApplicationRecord
     belongs_to :event
-    has_many :guests, class_name: 'User'
+    belongs_to :host, class_name: "User"
+    has_many :guest_invitings, foreign_key: :guesting_invitation_id
+    has_many :guests, through: :guest_invitings, source: :guest_invited
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   has_many :event_attendings, foreign_key: :event_attendee_id
   has_many :attended_events, through: :event_attendings
-  has_many :invitations, foreign_key: :guest_id
+  has_many :hosting_invitations, foreign_key: :host_id, class_name: "Event"
+  has_many :guest_invitings, foreign_key: :guest_invited_id
+  has_many :guesting_invitations, through: :guest_invitings
   
   validates :username, presence: true,uniqueness: true, length: { minimum: 5}
 end
